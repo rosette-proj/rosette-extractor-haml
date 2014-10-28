a58719ca48e874b3ca4351eaa0cb7aecf159c37b
Cameron Dutro <camertron@gmail.com>
2014-10-27 22:41:57 -0700

    Fix haml patch
    Haml patch identifies while and until statements, was broken.

--- lib/rosette/extractors/haml-extractor/haml_patch.rb
+++ lib/rosette/extractors/haml-extractor/haml_patch.rb
@@ -7,7 +7,10 @@ module Haml
 
     # Patch keywords until https://github.com/haml/haml/pull/802
     # and/or https://github.com/haml/haml/pull/803 get merged.
+    # Total hack. Don't try this at home, kids.
     START_BLOCK_KEYWORDS = %w[if begin case unless while until]
+    START_BLOCK_KEYWORD_REGEX = /(?:\w+(?:,\s*\w+)*\s*=\s*)?(#{START_BLOCK_KEYWORDS.join('|')})/
+    BLOCK_KEYWORD_REGEX = /^-?\s*(?:(#{MID_BLOCK_KEYWORDS.join('|')})|#{START_BLOCK_KEYWORD_REGEX.source})\b/
 
     $VERBOSE = original_verbosity
   end
